bc8620714b12b2e5cff74db8fbf20bef8afb1e12
nurullah911 <nurullah.ermagambet@narxoz.kz>
2025-04-08 15:29:26 +0500

    simple bg

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,13 +10,14 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; //16x16
     final int scale = 3;
     public final int tileSize = originalTileSize*scale;//48x48
-    final int maxScreenCol = 16;
-    final int maxScreenRaw = 12;
-    final int screenWidth = tileSize*maxScreenCol; //768 pixels
-    final int screenHeight = tileSize*maxScreenRaw; // 576 pixels
+    public final int maxScreenCol = 16;
+    public final int maxScreenRaw = 12;
+    public final int screenWidth = tileSize*maxScreenCol; //768 pixels
+    public final int screenHeight = tileSize*maxScreenRaw; // 576 pixels
     //FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this,keyH);
@@ -69,6 +71,7 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
+        tileM.draw(g2);
         player.draw(g2);
         g2.dispose();
     }
